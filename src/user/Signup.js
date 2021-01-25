4f7e37557bf1dc4f0937ddd690f2e48d4facc703
Helena Johansson <helena@klarr.se>
2021-01-25 12:01:08 +0100

    signup component

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -3,7 +3,101 @@ import { Link } from "react-router-dom";
 import { signup } from "../auth";
 
 const Signup = () => {
-  return <div className="layout">Signup</div>;
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: null,
+    success: false,
+  });
+
+  const { name, email, password, success, error } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password }).then((data) => {
+      if (error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({
+          ...values,
+          name: "",
+          email: "",
+          password: "",
+          error: "",
+          success: true,
+        });
+      }
+    });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Namn</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+          value={name}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+          value={email}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Lösenord</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+          value={password}
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Skicka
+      </button>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: success ? "" : "none" }}
+    >
+      Nytt konto är skapat var god att <Link to="/signin">Logga In</Link>
+    </div>
+  );
+
+  return (
+    <div className="layout">
+      {showSuccess()}
+      {showError()}
+      {signUpForm()}
+    </div>
+  );
 };
 
 export default Signup;
